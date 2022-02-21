33af6570664d4abfb8b215044e970b035b1e72e0
ishifoev <alex.malikov94@gmail.com>
2022-02-21 11:37:56 +0500

    Working with Heroku Client

--- index.php
+++ index.php
@@ -1,25 +1,17 @@
 <?php
 
-$url = "https://api.heroku.com/account/invoices";
+require_once __DIR__ . '/vendor/autoload.php';
 
-$curl = curl_init($url);
-curl_setopt($curl, CURLOPT_URL, $url);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+use Ishifoev\HerokuApi\HerokuClient;
 
-$headers = array(
-   "Accept: application/vnd.heroku+json; version=3",
-   "Authorization: Bearer 204bb74b-b82d-4b88-a63b-ea01c0890da2",
-);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-//for debug only!
-curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$heroku = new HerokuClient([
+   'apiKey' => '204bb74b-b82d-4b88-a63b-ea01c0890da2',
+]);
 
-$resp = curl_exec($curl);
-curl_close($curl);
+$invoice = $heroku->get('account/invoices');
+
 echo '<pre>';
-var_export(json_decode($resp));
+var_export($invoice);
 echo '</pre>';
-
 ?>
 
