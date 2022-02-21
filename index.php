9630dffa7ab8245ed568e4d79c8424efbcc673c5
ishifoev <alex.malikov94@gmail.com>
2022-02-21 16:52:30 +0500

    Heroku Client add in index.php

--- index.php
+++ index.php
@@ -2,10 +2,33 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use Ishifoev\HerokuApi\HerokuGenerateJson;
+use Ishifoev\HerokuApi\HerokuClient;
 
-$json = new HerokuGenerateJson;
-$json->generateApiJson();
+$heroku = new HerokuClient([
+   'apiKey' => 'YOUR_API_HERE',
+]);
+
+//Account information
+$account = $heroku->get('account');
+echo '<pre>';
+var_export($account);
+echo '</pre>';
+
+file_put_contents("account.json", json_encode($account, JSON_PRETTY_PRINT));
+
+//Invoice information
+$invoice = $heroku->get('account/invoices');
+echo '<pre>';
+var_export($invoice);
+echo '</pre>';
+file_put_contents("invoice.json", json_encode($invoice, JSON_PRETTY_PRINT));
+
+//Invoice address
+$invoice_address = $heroku->get('account/invoice-address');
+echo '<pre>';
+var_export($invoice_address);
+echo '</pre>';
+file_put_contents("invoice_address.json", json_encode($invoice_address, JSON_PRETTY_PRINT));
 
 ?>
 
