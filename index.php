518ea816d690483cbd73957c546316d072120d0d
ishifoev <alex.malikov94@gmail.com>
2022-02-22 11:10:44 +0500

    Fixing problem

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Ishifoev\HerokuApi\HerokuClient;
 
 $heroku = new HerokuClient([
-   'apiKey' => 'YOUR_API_HERE',
+   'apiKey' => '204bb74b-b82d-4b88-a63b-ea01c0890da2',
 ]);
 
 //Account information
 $account = $heroku->get('account');
 echo '<pre>';
-var_export($account);
+var_dump($account);
 echo '</pre>';
 
 file_put_contents("account.json", json_encode($account, JSON_PRETTY_PRINT));
@@ -19,14 +19,14 @@ file_put_contents("account.json", json_encode($account, JSON_PRETTY_PRINT));
 //Invoice information
 $invoice = $heroku->get('account/invoices');
 echo '<pre>';
-var_export($invoice);
+var_dump($invoice);
 echo '</pre>';
 file_put_contents("invoice.json", json_encode($invoice, JSON_PRETTY_PRINT));
 
 //Invoice address
 $invoice_address = $heroku->get('account/invoice-address');
 echo '<pre>';
-var_export($invoice_address);
+var_dump($invoice_address);
 echo '</pre>';
 file_put_contents("invoice_address.json", json_encode($invoice_address, JSON_PRETTY_PRINT));
 
